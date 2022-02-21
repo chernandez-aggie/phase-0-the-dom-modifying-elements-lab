690c455ccece71cb62912dbb0c43cc5d4af3c174
candacehernandez <chernandez.aggie@gmail.com>
2022-02-21 11:00:24 -0600

    commit tree 8a333bfbe64c3da19294d8795a262204ea099495

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector('#main').remove();
+
+let newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+document.body.append(newHeader);
+
+newHeader.innerHTML = "\"YOUR-NAME is the champion\"";
